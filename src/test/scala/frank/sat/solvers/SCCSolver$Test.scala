105741ce3b1269baa83996d5ea81b594886548b0
Frank Vega <vega.frank@gmail.com>
2019-08-07 10:28:50 -0400

    Added SAT structure

---
+++ src/test/scala/frank/sat/solvers/SCCSolver$Test.scala
@@ -0,0 +1,12 @@
+package frank.sat.solvers
+
+import frank.sat._
+
+import org.scalatest.FlatSpec
+
+class SCCSolver$Test extends FlatSpec with Common {
+
+  private val solver = SCCSolver
+
+  behavior of "SCCSolver$"
+}
